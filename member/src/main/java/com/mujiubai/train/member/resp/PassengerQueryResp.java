bd1735850e8e42ff3b74e56122971ceea350e121
mujiubai <1057378931@qq.com>
2023-11-02 16:04:05 +0800

    格式化返回的日期字段

--- member/src/main/java/com/mujiubai/train/member/resp/PassengerQueryResp.java
+++ member/src/main/java/com/mujiubai/train/member/resp/PassengerQueryResp.java
@@ -2,6 +2,8 @@ package com.mujiubai.train.member.resp;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 public class PassengerQueryResp {
     private Long id;
 
@@ -13,8 +15,10 @@ public class PassengerQueryResp {
 
     private String type;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createTime;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date updateTime;
 
     public Long getId() {
