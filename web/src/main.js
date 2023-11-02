bad13719842c5ec05feaf6b651d13ad4df846869
mujiubai <1057378931@qq.com>
2023-11-02 20:46:46 +0800

    添加乘车人删除前端页面

--- web/src/main.js
+++ web/src/main.js
@@ -7,6 +7,7 @@ import 'ant-design-vue/dist/antd.css';
 import * as Icons from '@ant-design/icons-vue'
 import axios from 'axios'
 import { notification } from 'ant-design-vue';
+import './assets/js/enums.js';
 
 const app=createApp(App);
 app.use(Antd).use(store).use(router).mount('#app');
--- web/src/views/main/passenger.vue
+++ web/src/views/main/passenger.vue
@@ -42,9 +42,9 @@
       </a-form-item>
       <a-form-item label="旅客类型">
         <a-select v-model:value="passenger.type">
-          <a-select-option value="1">成人</a-select-option>
-          <a-select-option value="2">儿童</a-select-option>
-          <a-select-option value="3">学生</a-select-option>
+          <a-select-option v-for="item in PASSENGER_TYPE_ARRAY" :key="item.code" :value="item.code">
+            {{item.desc}}
+          </a-select-option>
         </a-select>
       </a-form-item>
     </a-form>
