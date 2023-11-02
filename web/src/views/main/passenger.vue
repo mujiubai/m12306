e950994620d14cf89d5cfcd334de8cec116cbffd
mujiubai <1057378931@qq.com>
2023-11-02 11:16:14 +0800

    增加前端添加乘客页面开发

--- web/src/views/main/passenger.vue
+++ web/src/views/main/passenger.vue
@@ -1,15 +1,181 @@
 <template>
-  <h1>乘车人页面</h1>
+  <p>
+    <a-space>
+      <a-button type="primary" @click="onAdd">新增</a-button>
+    </a-space>
+  </p>
+  <a-modal v-model:visible="visible" title="乘车人" @ok="handleOk"
+           ok-text="确认" cancel-text="取消">
+    <a-form :model="passenger" :label-col="{span: 4}" :wrapper-col="{ span: 20 }">
+      <a-form-item label="姓名">
+        <a-input v-model:value="passenger.name" />
+      </a-form-item>
+      <a-form-item label="身份证">
+        <a-input v-model:value="passenger.idCard" />
+      </a-form-item>
+      <a-form-item label="旅客类型">
+        <a-select v-model:value="passenger.type">
+          <a-select-option value="1">成人</a-select-option>
+          <a-select-option value="2">儿童</a-select-option>
+          <a-select-option value="3">学生</a-select-option>
+        </a-select>
+      </a-form-item>
+    </a-form>
+  </a-modal>
 </template>
+
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent, ref, onMounted } from 'vue';
+import {notification} from "ant-design-vue";
+import axios from "axios";
 
 export default defineComponent({
+  name: "passenger-view",
   setup() {
+    const PASSENGER_TYPE_ARRAY = window.PASSENGER_TYPE_ARRAY;
+    const visible = ref(false);
+    let passenger = ref({
+      id: undefined,
+      memberId: undefined,
+      name: undefined,
+      idCard: undefined,
+      type: undefined,
+      createTime: undefined,
+      updateTime: undefined,
+    });
+    const passengers = ref([]);
+    // 分页的三个属性名是固定的
+    const pagination = ref({
+      total: 0,
+      current: 1,
+      pageSize: 10,
+    });
+    let loading = ref(false);
+    const columns = [
+    {
+      title: '会员id',
+      dataIndex: 'memberId',
+      key: 'memberId',
+    },
+    {
+      title: '姓名',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: '身份证',
+      dataIndex: 'idCard',
+      key: 'idCard',
+    },
+    {
+      title: '旅客类型',
+      dataIndex: 'type',
+      key: 'type',
+    },
+    {
+      title: '操作',
+      dataIndex: 'operation'
+    }
+    ];
+
+    const onAdd = () => {
+      passenger.value = {};
+      visible.value = true;
+    };
+
+    const onEdit = (record) => {
+      passenger.value = window.Tool.copy(record);
+      visible.value = true;
+    };
+
+    const onDelete = (record) => {
+      axios.delete("/member/passenger/delete/" + record.id).then((response) => {
+        const data = response.data;
+        if (data.success) {
+          notification.success({description: "删除成功！"});
+          handleQuery({
+            page: pagination.value.current,
+            size: pagination.value.pageSize,
+          });
+        } else {
+          notification.error({description: data.message});
+        }
+      });
+    };
+
+    const handleOk = () => {
+      axios.post("/member/passenger/save", passenger.value).then((response) => {
+        let data = response.data;
+        if (data.success) {
+          notification.success({description: "保存成功！"});
+          visible.value = false;
+          handleQuery({
+            page: pagination.value.current,
+            size: pagination.value.pageSize
+          });
+        } else {
+          notification.error({description: data.message});
+        }
+      });
+    };
+
+    const handleQuery = (param) => {
+      if (!param) {
+        param = {
+          page: 1,
+          size: pagination.value.pageSize
+        };
+      }
+      loading.value = true;
+      axios.get("/member/passenger/query-list", {
+        params: {
+          page: param.page,
+          size: param.size
+        }
+      }).then((response) => {
+        loading.value = false;
+        let data = response.data;
+        if (data.success) {
+          passengers.value = data.content.list;
+          // 设置分页控件的值
+          pagination.value.current = param.page;
+          pagination.value.total = data.content.total;
+        } else {
+          notification.error({description: data.message});
+        }
+      });
+    };
+
+    const handleTableChange = (pagination) => {
+      // console.log("看看自带的分页参数都有啥：" + pagination);
+      handleQuery({
+        page: pagination.current,
+        size: pagination.pageSize
+      });
+    };
+
+    onMounted(() => {
+      handleQuery({
+        page: 1,
+        size: pagination.value.pageSize
+      });
+    });
+
     return {
+      PASSENGER_TYPE_ARRAY,
+      passenger,
+      visible,
+      passengers,
+      pagination,
+      columns,
+      handleTableChange,
+      handleQuery,
+      loading,
+      onAdd,
+      handleOk,
+      onEdit,
+      onDelete
     };
   },
 });
 </script>
-<style>
-</style>
