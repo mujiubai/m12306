a7e24478e603e810e4a035dc0b91000cd9ac380c
mujiubai <1057378931@qq.com>
2023-11-07 17:31:15 +0800

    增加车次、车厢、座位按条件查询接口

--- business/src/main/java/com/mujiubai/train/business/req/TrainCarriageQueryReq.java
+++ business/src/main/java/com/mujiubai/train/business/req/TrainCarriageQueryReq.java
@@ -3,10 +3,18 @@ package com.mujiubai.train.business.req;
 import com.mujiubai.train.common.req.PageReq;
 
 public class TrainCarriageQueryReq extends PageReq {
+    private String trainCode;
 
     @Override
     public String toString() {
-        return "TrainCarriageQueryReq{" +
-                "} " + super.toString();
+        return "TrainCarriageQueryReq [trainCode=" + trainCode + "]";
+    }
+
+    public String getTrainCode() {
+        return trainCode;
+    }
+
+    public void setTrainCode(String trainCode) {
+        this.trainCode = trainCode;
     }
 }
--- business/src/main/java/com/mujiubai/train/business/req/TrainSeatQueryReq.java
+++ business/src/main/java/com/mujiubai/train/business/req/TrainSeatQueryReq.java
@@ -3,10 +3,18 @@ package com.mujiubai.train.business.req;
 import com.mujiubai.train.common.req.PageReq;
 
 public class TrainSeatQueryReq extends PageReq {
+    private String trainCode;
 
     @Override
     public String toString() {
-        return "TrainSeatQueryReq{" +
-                "} " + super.toString();
+        return "TrainSeatQueryReq [trainCode=" + trainCode + "]";
+    }
+
+    public String getTrainCode() {
+        return trainCode;
+    }
+
+    public void setTrainCode(String trainCode) {
+        this.trainCode = trainCode;
     }
 }
