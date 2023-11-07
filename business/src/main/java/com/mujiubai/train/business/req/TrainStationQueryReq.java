af35f79e72ab0c9127fb27d3c8afd31d39801b8f
mujiubai <1057378931@qq.com>
2023-11-07 15:56:35 +0800

    添加火车车站表，生成持久层和前后端代码

---
+++ business/src/main/java/com/mujiubai/train/business/req/TrainStationQueryReq.java
@@ -0,0 +1,12 @@
+package com.mujiubai.train.business.req;
+
+import com.mujiubai.train.common.req.PageReq;
+
+public class TrainStationQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "TrainStationQueryReq{" +
+                "} " + super.toString();
+    }
+}
