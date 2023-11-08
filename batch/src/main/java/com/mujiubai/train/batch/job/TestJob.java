0fb6e34119ea49bbabc90c349dbf6704cabfc534
mujiubai <1057378931@qq.com>
2023-11-08 16:45:09 +0800

    继承quartz，测试quartz和springboot自带的定时调度

---
+++ batch/src/main/java/com/mujiubai/train/batch/job/TestJob.java
@@ -0,0 +1,16 @@
+package com.mujiubai.train.batch.job;
+
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+@DisallowConcurrentExecution
+public class TestJob implements Job {
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        System.out.println("TestJob");
+
+    }
+}
