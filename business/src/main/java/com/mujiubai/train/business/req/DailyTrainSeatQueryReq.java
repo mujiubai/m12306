8db61d3815ab12c7745176cbbed890103b828e2e
mujiubai <1057378931@qq.com>
2023-11-10 20:35:13 +0800

    添加daily-train-seat表，生成持久层和前后端代码，并增加车次查询条件

---
+++ business/src/main/java/com/mujiubai/train/business/req/DailyTrainSeatQueryReq.java
@@ -0,0 +1,20 @@
+package com.mujiubai.train.business.req;
+
+import com.mujiubai.train.common.req.PageReq;
+
+public class DailyTrainSeatQueryReq extends PageReq {
+    private String trainCode;
+
+    @Override
+    public String toString() {
+        return "DailyTrainSeatQueryReq [trainCode=" + trainCode + "]";
+    }
+
+    public String getTrainCode() {
+        return trainCode;
+    }
+
+    public void setTrainCode(String trainCode) {
+        this.trainCode = trainCode;
+    }
+}
