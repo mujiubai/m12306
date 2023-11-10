5654fc7d5343a34e00e8f06f153af4987819fbdd
mujiubai <1057378931@qq.com>
2023-11-10 19:36:53 +0800

    添加daily-train-station表，生成持久层和前后端代码，并增加日期、车次查询条件

---
+++ business/src/main/java/com/mujiubai/train/business/controller/admin/DailyTrainStationAdminController.java
@@ -0,0 +1,38 @@
+package com.mujiubai.train.business.controller.admin;
+
+import com.mujiubai.train.common.resp.CommonResp;
+import com.mujiubai.train.common.resp.PageResp;
+import com.mujiubai.train.business.req.DailyTrainStationQueryReq;
+import com.mujiubai.train.business.req.DailyTrainStationSaveReq;
+import com.mujiubai.train.business.resp.DailyTrainStationQueryResp;
+import com.mujiubai.train.business.service.DailyTrainStationService;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin/daily-train-station")
+public class DailyTrainStationAdminController {
+    @Resource
+    private DailyTrainStationService dailyTrainStationService;
+
+    @PostMapping("/save")
+    public CommonResp<Object> save(@Valid @RequestBody DailyTrainStationSaveReq req) {
+        dailyTrainStationService.save(req);
+        return new CommonResp<>();
+    }
+
+    @GetMapping("/query-list")
+    public CommonResp<PageResp<DailyTrainStationQueryResp>> queryList(@Valid DailyTrainStationQueryReq req) {
+        PageResp<DailyTrainStationQueryResp> list = dailyTrainStationService.queryList(req);
+        return new CommonResp<>(list);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp<Object> delete(@PathVariable Long id) {
+        dailyTrainStationService.delete(id);
+        return new CommonResp<>();
+    }
+}
