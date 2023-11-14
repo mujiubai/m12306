731aabb588cbda283d8fe4546d7c283f7235537c
mujiubai <1057378931@qq.com>
2023-11-14 16:15:08 +0800

    集成feign，实现模块间调用。为各模块添加名字

--- batch/src/main/java/com/mujiubai/train/batch/config/BatchApplication.java
+++ batch/src/main/java/com/mujiubai/train/batch/config/BatchApplication.java
@@ -6,12 +6,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.mujiubai")
 @MapperScan("com.mujiubai.train.batch.mapper")
+@EnableFeignClients("com.mujiubai.train.batch.feign")
 public class BatchApplication {
 	private static final Logger LOG = LoggerFactory.getLogger(BatchApplication.class);
 	public static void main(String[] args) {
--- batch/src/main/java/com/mujiubai/train/batch/controller/BatchController.java
+++ batch/src/main/java/com/mujiubai/train/batch/controller/BatchController.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mujiubai.train.batch.feign.BusinessFeign;
 import com.mujiubai.train.common.resp.CommonResp;
+
+import jakarta.annotation.Resource;
 
 
 // import jakarta.annotation.Resource;
@@ -20,9 +23,12 @@ public class BatchController {
 
     // @Resource
     // private BusinessController memberService;
+    @Resource
+    BusinessFeign businessFeign;
 
     @GetMapping("/hello")
     public CommonResp<String> hello() {
+        String ans=businessFeign.hello();
         CommonResp<String> commonResp=new CommonResp<>();
         commonResp.setContent("hello world! batch");
         return commonResp;
