ddd67c9ecaaf8ab727ddabe87990cfa47a9bbba2
mujiubai <1057378931@qq.com>
2023-11-15 11:43:05 +0800

    增加余票表，并生成相应持久层和前后端代码

---
+++ business/src/main/java/com/mujiubai/train/business/req/DailyTrainTicketQueryReq.java
@@ -0,0 +1,12 @@
+package com.mujiubai.train.business.req;
+
+import com.mujiubai.train.common.req.PageReq;
+
+public class DailyTrainTicketQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "DailyTrainTicketQueryReq{" +
+                "} " + super.toString();
+    }
+}
