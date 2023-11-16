4d2023335191bc648b76486c8ed1da3d0fbf583c
mujiubai <1057378931@qq.com>
2023-11-16 21:04:49 +0800

    添加每日车票唯一键查询入口，为business增加成员拦截器

--- business/src/main/java/com/mujiubai/train/business/service/DailyTrainTicketService.java
+++ business/src/main/java/com/mujiubai/train/business/service/DailyTrainTicketService.java
@@ -170,4 +170,16 @@ public class DailyTrainTicketService {
         LOG.info("生成日期【{}】车次【{}】的余票信息结束", DateUtil.formatDate(date), trainCode);
 
     }
+
+    public DailyTrainTicket selectByUnique(Date date, String trainCode, String start, String end) {
+        DailyTrainTicketExample DailyTrainTicketExample = new DailyTrainTicketExample();
+        DailyTrainTicketExample.createCriteria().andDateEqualTo(date).andTrainCodeEqualTo(trainCode)
+                .andStartEqualTo(start).andEndEqualTo(end);
+        List<DailyTrainTicket> list = dailyTrainTicketMapper.selectByExample(DailyTrainTicketExample);
+        if (CollUtil.isNotEmpty(list)) {
+            return list.get(0);
+        } else {
+            return null;
+        }
+    }
 }
